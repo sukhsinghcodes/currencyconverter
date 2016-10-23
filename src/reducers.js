57e65ffa169a65e2e85f11822541e65ee6d3e02e
Sukhvinder Singh Durhailay <sukhvinder.durhailay@bostonivy.co>
2016-10-23 17:37:22 +0100

    Added calculation of result value

--- src/reducers.js
+++ src/reducers.js
@@ -6,7 +6,7 @@ function reducer(state = {}, action) {
 		case ActionTypes.SELECT_BASE_CURRENCY:
 			return { ...state, base: action.currency };
 		case ActionTypes.SELECT_RESULT_CURRENCY:
-			return { ...state, result: action.currency };
+			return { ...state, result: action.currency, resultValue: (state.baseValue*state.rates[action.currency]).toFixed(2) };
 		case ActionTypes.FETCH_RATES:
 			return { ...state, isFetching: true };
 		case ActionTypes.FETCH_RATES_SUCCESS:
@@ -20,7 +20,7 @@ function reducer(state = {}, action) {
 				currencies: currencies, 
 				result: resultCurrency, 
 				resultValue: action.data.rates[resultCurrency].toFixed(2), 
-				baseValue: 1.00 
+				baseValue: 1.00
 			};
 		case ActionTypes.FETCH_RATES_FAIL:
 			return { ...state, error: action.error };
